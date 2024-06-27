72d1d6abafaf25a07e4a17b985ba45834269aac1
ernesto-official02 <ernestanmol@gmail.com>
2024-06-27 13:08:57 +0530

    task1

---
+++ Javascript/demo1.js
@@ -0,0 +1,29 @@
+ console.log(2+3)
+
+ var x=10;
+ var y = 20;
+ z=90;
+  
+
+ hello= "hi everyone";
+ console.log(hello);
+
+ const pi_value=3.142;
+ console.log(pi_value);
+
+ const direction = "left";
+ {
+    console.log(direction);
+ }
+
+ for(let i =0; i<5;i++)
+    {
+        console.log(i)
+    }
+
+    var first=10;
+    var second= 20;
+    second+=first;
+    console.log(second)
+
+    console.log(2**3)
---
+++ Javascript/index.js
@@ -0,0 +1,21 @@
+// introduction to javascript
+
+console.log("hello")
+
+var a =9;
+var b=9;
+
+console.log(a+b)
+
+// //there is the  
+// es6 javascript
+var squareresult = square(4);
+console.log(squareresult)
+
+console.log("hello")
+var n =2;     //local variable 
+function square(num)
+{
+    return num*num;
+}
+document
